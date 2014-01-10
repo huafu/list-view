561a63d18e143d38a3172f365121a5abe33fd9d4
Huafu Gandon <huafu.gandon@gmail.com>
2014-01-10 14:07:39 +0700

    Allow mouse wheel to scroll horizontal list when no data coming from X axis (mouse with only one scroll direction)

--- packages/list-view/lib/virtual_list_view.js
+++ packages/list-view/lib/virtual_list_view.js
@@ -179,7 +179,9 @@ Ember.VirtualListView = Ember.ContainerView.extend(Ember.ListViewMixin, Ember.Vi
     var inverted, delta, candidatePosition, isHoriz = get(this, 'isHorizontal');
 
     inverted = e.webkitDirectionInvertedFromDevice;
-    delta = e[isHoriz ? 'wheelDeltaX' : 'wheelDeltaY'] * (inverted ? 0.8 : -0.8);
+    delta = e[isHoriz ? 'wheelDeltaX' : 'wheelDeltaY'];
+    if ( isHoriz && !delta ) delta = e.wheelDeltaY;
+    delta = delta * (inverted ? 0.8 : -0.8);
     candidatePosition = this.scroller[isHoriz ? '__scrollLeft' : '__scrollTop'] + delta;
 
     if ((candidatePosition >= 0) && (candidatePosition <= this.scroller[isHoriz ? '__maxScrollLeft' : '__maxScrollTop'])) {
